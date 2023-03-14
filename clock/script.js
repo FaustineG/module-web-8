b1984d46e475bf99d1a1ee6b0dcf7106acbc536d
faustine.godbillot <faustine.godbillot@zenika.com>
2023-03-14 14:11:43 +0100

    clock start and stop

--- clock/script.js
+++ clock/script.js
@@ -1,14 +1,29 @@
 // récuperer elements HTML
 let myElement = document.getElementById("clock");
-let buttonStart = document.getElementById("start");
-let buttonStop = document.getElementById("stop");
+let myStartAndStopButton = document.getElementById("start-and-stop");
+function randomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
 
+function changeColor() {
+  const doc = document.getElementsByTagName('body')[0];
+  // myElement.style = "background-color:#" + randomColor() + ';';
+  doc.style = `background-color:#${randomColor()}`;
+}
+
+/**
+ * Puts current time in the "clock" div
+ */
 function displayCurrentTime() {
   let today = new Date();
 
   let hours = today.getHours();
   let minutes = today.getMinutes();
   let seconds = today.getSeconds();
+
+  if (seconds % 10 == 0) {
+    changeColor();
+  }
 
   myElement.innerText = hours + ":" + minutes + ":" + seconds;
 }
@@ -20,9 +35,25 @@ let intervalId = setInterval(displayCurrentTime, 1000);
 function onStopClick() {
   clearInterval(intervalId);
 }
-buttonStop.addEventListener("click", onStopClick);
 
 function onStartClick() {
   intervalId = setInterval(displayCurrentTime, 1000);
 }
-buttonStart.addEventListener("click", onStartClick);
+
+let isStarted = true;
+
+function startAndStop() {
+  if (isStarted == true) {
+    onStopClick();
+    myStartAndStopButton.classList.add("start");
+    myStartAndStopButton.classList.remove("stop");
+    myStartAndStopButton.innerText = "START";
+    isStarted = false;
+  } else {
+    onStartClick();
+    myStartAndStopButton.classList.add("stop");
+    myStartAndStopButton.classList.remove("start");
+    myStartAndStopButton.innerText = "STOP";
+    isStarted = true;
+  }
+}
