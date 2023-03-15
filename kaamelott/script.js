2b61a58f98c3aeb185f4584d0279015530647111
faustine.godbillot <faustine.godbillot@zenika.com>
2023-03-15 15:50:36 +0100

    kaamelott done

--- kaamelott/script.js
+++ kaamelott/script.js
@@ -1,13 +1,33 @@
-const quotesElements = document.getElementById("quotes");
+// Je récpère mon élément racine
+const quotesListElement = document.getElementById("quotes-list");
 
-function fetchQuote() {
-  fetch("http://localhost:8080/quote")
+async function fetchQuote() {
+  return fetch("http://localhost:8080/quote")
     .then(function parseResponse(response) {
       return response.json();
     })
     .then(function doSomething(data) {
       const { quote, personnage, saison, episode } = data;
-      quotesElements.innerText = quote;
-      console.log(data, quote, personnage, saison, episode);
+      const parentElement = document.createElement("div");
+
+      // Ajout dynamique d'un <p> dans une liste
+      // Je crée un p programmatiquement
+      const quoteElement = document.createElement("p");
+      quoteElement.classList.add('quote')
+      quoteElement.innerText = quote;
+    //   quoteElement.innerText = `"${quote}"`;
+    //   quoteElement.innerText = '"' + quote+'"';
+
+      // Je met mon <p> dans mon element racine
+      const infoElement = document.createElement("p");
+      infoElement.classList.add('information')
+      infoElement.innerText = `Dit par ${personnage} dans l'épisode ${episode}`;
+
+      parentElement.appendChild(quoteElement);
+      parentElement.appendChild(infoElement);
+
+      quotesListElement.appendChild(parentElement)
+
+      return data;
     });
 }
